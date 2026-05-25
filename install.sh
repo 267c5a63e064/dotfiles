8f2a4b4777832d9e6103bf588a19e29a26530a5e
267c5a63e064 <94622983+267c5a63e064@users.noreply.github.com>
2026-05-25 21:39:21 +0200

    feat: lapce settings

--- install.sh
+++ install.sh
@@ -3,11 +3,15 @@ set -e
 
 DOTFILES="$HOME/dotfiles"
 
+# Bash
 ln -sf "$DOTFILES/bash/bashrc" "$HOME/.bashrc"
 ln -sf "$DOTFILES/bash/bash_profile" "$HOME/.bash_profile"
 
+# Ghostty
 mkdir -p "$HOME/.config/ghostty"
-
 ln -sf "$DOTFILES/ghostty/config" "$HOME/.config/ghostty/config"
-
 ln -sf "$DOTFILES/ghostty/basic.config" "$HOME/.config/ghostty/basic.config"
+
+# Lapce
+mkdir -p "$HOME/.config/lapce"
+ln -sf "$DOTFILES/lapce/settings.toml" "$HOME/.var/app/dev.lapce.lapce/config/lapce-stable/settings.toml"
