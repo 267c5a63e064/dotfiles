2eab81fe02aff41b3f29cac90bdf579e5a50f5fe
267c5a63e064 <94622983+267c5a63e064@users.noreply.github.com>
2026-05-25 21:26:50 +0200

    chore: improvements

--- install.sh
+++ install.sh
@@ -7,4 +7,7 @@ ln -sf "$DOTFILES/bash/bashrc" "$HOME/.bashrc"
 ln -sf "$DOTFILES/bash/bash_profile" "$HOME/.bash_profile"
 
 mkdir -p "$HOME/.config/ghostty"
+
 ln -sf "$DOTFILES/ghostty/config" "$HOME/.config/ghostty/config"
+
+ln -sf "$DOTFILES/ghostty/basic.config" "$HOME/.config/ghostty/basic.config"
